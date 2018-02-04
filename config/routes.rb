948244a5edc57a775c99f1c383e91410590b9418
Herbert Ilhan Tanujaya <mail@herbert.id>
2018-02-04 17:00:02 +0800

    Add root in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root 'home#index'
   devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
