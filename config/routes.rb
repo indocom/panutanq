b39ddd68e3ebf5fe68cb205370b8cf2caf226acb
Hugo Herdianto <hugoherdianto@yahoo.com>
2018-02-11 17:51:10 +0800

    seeds

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@
 
 Rails.application.routes.draw do
   root 'home#index'
-  
+
   devise_for :users
   resources :users, except: :create
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  post 'create_user' => 'users#create', as: :create_user      
+  post 'create_user' => 'users#create', as: :create_user
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # == Schema Information
 #
 # Table name: users
@@ -25,6 +27,5 @@
 
 FactoryBot.define do
   factory :user do
-    
   end
 end
