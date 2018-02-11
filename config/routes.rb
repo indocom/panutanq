ede639815b5e53b513bdd4ee5861b82cc85e4320
Calvin Tantio <calvin.tantio@yahoo.com>
2018-02-11 15:10:03 +0800

    Rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  
   root 'feedbacks#index'
   resources :feedbacks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
-
