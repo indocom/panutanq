805f7a683cac2381fa114738779d15ea2b76faea
Ryvan Firdy <ryvanfirdy@gmail.com>
2018-02-17 19:08:51 +0700

    manage accounts instead of delete your account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,14 @@
 class UsersController < ApplicationController
   #	before_action :authorize_admin, only: :create
 
+  def index
+    @users = User.all
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end  
+  
   def new
     @user = User.new
   end
