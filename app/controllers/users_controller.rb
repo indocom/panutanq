d510271fddb45a9f2a09c934a88338c2095d0575
Ryvan Firdy <ryvanfirdy@gmail.com>
2018-02-17 19:45:36 +0700

    generate cancancan model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,11 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
-  
+
   def show
     @user = User.find(params[:id])
-  end  
-  
+  end
+
   def new
     @user = User.new
   end
