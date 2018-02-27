e0801b2269810d13013af7d6f6dbfc284fbb2719
xqlz <zefangelica@gmail.com>
2018-02-27 18:27:32 +0800

    modified indentations

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
   get 'home', to: 'static_pages#home'
+  get 'about', to: 'static_pages#about'
 end
