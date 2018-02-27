36dcd8d37eea6286b211b860a8a8420f2186d122
Herbert Ilhan Tanujaya <mail@herbert.id>
2018-02-27 11:43:36 +0800

    Rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   # before_action :configure_sign_up_params, only: [:create]
-  before_action :configure_account_update_params, only: [:update]
+  # before_action :configure_account_update_params, only: [:update]
 
   # Catch CanCan:AccessDenied exception
   rescue_from CanCan::AccessDenied do |_exception|
@@ -18,9 +18,9 @@ class ApplicationController < ActionController::Base
   # end
 
   # If you have extra params to permit, append them to the sanitizer.
-  def configure_account_update_params
-    devise_parameter_sanitizer.permit(
-      :account_update, keys: %i[attribute fullname]
-    )
-  end
+  # def configure_account_update_params
+  #   devise_parameter_sanitizer.permit(
+  #     :account_update, keys: %i[attribute fullname]
+  #   )
+  # end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,6 @@
 class UsersController < ApplicationController
   load_and_authorize_resource
 
-
   def index
     @users = User.all
   end
