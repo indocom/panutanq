45b57f657d7e0de4bc46f62125939b705c3e650a
Herbert Ilhan Tanujaya <mail@herbert.id>
2018-02-27 22:59:17 +0800

    Misc changes

--- db/migrate/20180227083647_add_display_columns_for_users.rb
+++ db/migrate/20180227083647_add_display_columns_for_users.rb
@@ -1,7 +1,7 @@
 class AddDisplayColumnsForUsers < ActiveRecord::Migration[5.1]
   def change
-  	add_column :users, :display_email, :boolean
-  	add_column :users, :display_contact_number, :boolean
+    add_column :users, :display_email, :boolean
+    add_column :users, :display_contact_number, :boolean
     add_column :users, :display_major, :boolean
     add_column :users, :display_graduation, :boolean
     add_column :users, :display_overseas_experience, :boolean
