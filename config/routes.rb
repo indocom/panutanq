9054afd922f241d891e8e5993a3ba2b2747b0a58
xqlz <zefangelica@gmail.com>
2018-02-27 17:41:10 +0800

    Add homepage for the 1st time

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root 'feedbacks#index'
+  resources :feedbacks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static_pages#home'
+  get 'home', to: 'static_pages#home'
 end
