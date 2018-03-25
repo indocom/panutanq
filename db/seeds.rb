84325e9e7ef8548a2ffb15925bbac607a2e68ea7
Hugo Herdianto <hugoherdianto@yahoo.com>
2018-03-25 17:58:02 +0800

    Add sample description to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,7 +20,8 @@ User.create(fullname: 'abcd', email: 'abcd@gmail.com', password: 'user123')
 
 category = Category.create(name: 'default')
 
-Event.create(name: 'NUANSA', category: category)
+Event.create(name: 'NUANSA', category: category, description: "
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pellentesque enim eu maximus dapibus. Sed semper leo a arcu dapibus maximus. Donec nec velit ut velit finibus interdum laoreet vel leo. Nulla tempor turpis sed orci suscipit laoreet. Etiam nec nibh tellus. Ut ut nulla at justo tristique dapibus et eget diam. Cras interdum nulla orci, at auctor sem fermentum ac. Donec nec vulputate sem, ut finibus purus. Cras id odio id diam rutrum feugiat id ac odio. Praesent vitae odio arcu. Phasellus placerat nibh sed ex dictum malesuada. Quisque efficitur, est ac porttitor mattis, nisl dolor ullamcorper elit, eu euismod arcu leo ut tortor. Duis ac venenatis libero, suscipit imperdiet nunc. Nam non rutrum metus, ac sagittis augue. Maecenas vel nisi vel magna elementum commodo. Proin cursus felis a augue luctus hendrerit eget quis nulla.")
 Event.create(name: 'Misi Kami Peduli', category: category)
 Event.create(name: 'Orientation', category: category)
 Event.create(name: 'POPI', category: category)
