d50de06eaae7d09161853cf981f5dfc94bf1153c
Herbert Ilhan Tanujaya <mail@herbert.id>
2018-04-06 23:54:12 +0800

    Add Coveralls

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,6 +11,7 @@ end
 require 'rspec/rails'
 require 'support/factory_bot'
 # Add additional requires below this line. Rails is not loaded until this point!
+require 'support/coveralls'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
