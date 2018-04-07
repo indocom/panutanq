a5ecb6dd42826c3ef998c54036c11e35c8e8b2a6
Hugo Herdianto <hugoherdianto@yahoo.com>
2018-04-08 01:20:00 +0800

    Akhirnya bisa new event yey

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,4 +8,22 @@ class EventsController < ApplicationController
   def show
     @event = Event.find(params[:id])
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+  @event = Event.new(event_params)
+
+  if @event.save
+    redirect_to event_path(@event)
+    else
+    render :new
+    end
+  end
+
+  private def event_params
+    params.require(:event).permit(:category_id, :name, :description, :start_time, :end_time)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180227083647) do
+ActiveRecord::Schema.define(version: 20180407171000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,11 +24,11 @@ ActiveRecord::Schema.define(version: 20180227083647) do
   create_table "events", force: :cascade do |t|
     t.bigint   "category_id", :index=>{:name=>"index_events_on_category_id"}
     t.text     "description"
-    t.time     "start_time"
-    t.time     "end_time"
     t.string   "name"
     t.datetime "created_at",  :null=>false
     t.datetime "updated_at",  :null=>false
+    t.date     "start_time"
+    t.date     "end_time"
   end
 
   create_table "feedbacks", force: :cascade do |t|
