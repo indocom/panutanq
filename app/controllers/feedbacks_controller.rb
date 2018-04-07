aac9b05b477d503aef6678e1a495de8b42397e66
Hugo Herdianto <hugoherdianto@yahoo.com>
2018-04-08 00:41:53 +0800

    Au

---
+++ app/controllers/feedbacks_controller.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+class FeedbacksController < ApplicationController
+  before_action :set_feedback, only: %i[show destroy]
+
+  # GET /feedbacks
+  def index
+    @feedbacks = Feedback.all
+  end
+
+  # GET /feedbacks/1
+  def show; end
+
+  # GET /feedbacks/new
+  def new
+    @feedback = Feedback.new
+  end
+
+  # POST /feedbacks
+  def create
+    @feedback = Feedback.new(feedback_params)
+
+    if @feedback.anonymous == true
+      @feedback.name = 'Anonymous'
+      @feedback.email = 'Anonymous'
+    end
+
+    if @feedback.save
+      redirect_to @feedback, notice: 'Feedback was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  # DELETE /feedbacks/1
+  def destroy
+    @feedback.destroy
+    redirect_to feedbacks_url, notice: 'Feedback was successfully destroyed.'
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_feedback
+    @feedback = Feedback.find(params[:id])
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def feedback_params
+    params.require(:feedback).permit(:name, :email, :anonymous, :message)
+  end
+end
