b552bf44ec59120cae692fe43b536882cd3d4247
Ryvan Firdy <ryvanfirdy@gmail.com>
2018-04-12 21:47:32 +0800

    add pictures done

--- spec/factories/pictures.rb
+++ spec/factories/pictures.rb
@@ -5,7 +5,6 @@
 #
 #  id                   :integer          not null, primary key
 #  event_id             :integer
-#  post_id              :integer
 #  created_at           :datetime         not null
 #  updated_at           :datetime         not null
 #  picture_file_name    :string
@@ -16,12 +15,10 @@
 # Indexes
 #
 #  index_pictures_on_event_id  (event_id)
-#  index_pictures_on_post_id   (post_id)
 #
 # Foreign Keys
 #
 #  fk_rails_...  (event_id => events.id)
-#  fk_rails_...  (post_id => posts.id)
 #
 
 FactoryBot.define do
