552ad05d5ff0e59b10febfcb68b75fc00fedc896
Siska <siskaroselina@gmail.com>
2018-04-21 03:33:38 -0700

    last one

--- spec/models/feedback_spec.rb
+++ spec/models/feedback_spec.rb
@@ -1,13 +1,10 @@
 # frozen_string_literal: true
-
 # == Schema Information
 #
 # Table name: feedbacks
 #
 #  id         :integer          not null, primary key
-#  name       :string
 #  email      :string
-#  anonymous  :boolean          default(TRUE)
 #  message    :text
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
