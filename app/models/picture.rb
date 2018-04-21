9861afbd1775b324cc727462a0f667127a6fd64c
xqlz <zefangelica@gmail.com>
2018-04-21 11:31:02 +0800

    Add events dropdown class

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -5,6 +5,7 @@
 #
 #  id                   :integer          not null, primary key
 #  event_id             :integer
+#  post_id              :integer
 #  created_at           :datetime         not null
 #  updated_at           :datetime         not null
 #  picture_file_name    :string
@@ -15,10 +16,12 @@
 # Indexes
 #
 #  index_pictures_on_event_id  (event_id)
+#  index_pictures_on_post_id   (post_id)
 #
 # Foreign Keys
 #
 #  fk_rails_...  (event_id => events.id)
+#  fk_rails_...  (post_id => posts.id)
 #
 
 class Picture < ApplicationRecord
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20180409092834) do
 
   # These are extensions that must be enabled in order to support this database
@@ -43,12 +42,22 @@ ActiveRecord::Schema.define(version: 20180409092834) do
 
   create_table "pictures", force: :cascade do |t|
     t.bigint   "event_id",             :index=>{:name=>"index_pictures_on_event_id"}
+    t.bigint   "post_id",              :index=>{:name=>"index_pictures_on_post_id"}
     t.datetime "created_at",           :null=>false
     t.datetime "updated_at",           :null=>false
     t.string   "picture_file_name"
     t.string   "picture_content_type"
     t.integer  "picture_file_size"
     t.datetime "picture_updated_at"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.bigint   "category_id", :index=>{:name=>"index_posts_on_category_id"}
+    t.string   "name"
+    t.text     "description"
+    t.bigint   "event_id",    :index=>{:name=>"index_posts_on_event_id"}
+    t.datetime "created_at",  :null=>false
+    t.datetime "updated_at",  :null=>false
   end
 
   create_table "roles", force: :cascade do |t|
@@ -84,8 +93,6 @@ ActiveRecord::Schema.define(version: 20180409092834) do
     t.boolean  "display_graduation"
     t.boolean  "display_overseas_experience"
     t.boolean  "display_work_experience"
-    t.string   "provider"
-    t.string   "uid"
     t.string   "avatar_file_name"
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
@@ -101,4 +108,7 @@ ActiveRecord::Schema.define(version: 20180409092834) do
 
   add_foreign_key "events", "categories"
   add_foreign_key "pictures", "events"
+  add_foreign_key "pictures", "posts"
+  add_foreign_key "posts", "categories"
+  add_foreign_key "posts", "events"
 end
