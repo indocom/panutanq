3150db6c8defd1a9697d9d29f409f0d4627edc8b
Ryvan Firdy <ryvanfirdy@gmail.com>
2018-06-04 11:33:41 +0700

    modify controller

--- app/controllers/freshmen_controller.rb
+++ app/controllers/freshmen_controller.rb
@@ -1,2 +1,54 @@
 class FreshmenController < ApplicationController
+  load_and_authorize_resource
+
+  def index
+    @freshmens = Freshmen.all
+  end
+
+  def show
+    @freshmen = Freshmen.find(params[:id])
+  end
+
+  def new
+    @freshmen = Freshmen.new
+  end
+
+  def create
+    @freshmen = Freshmen.new(freshmen_params)
+
+    if @freshmen.save
+      redirect_to freshmen_path(@freshmen)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @freshmen = Freshmen.find(params[:id])
+  end
+
+  def update
+    @freshmen = Freshmen.find(params[:id])
+    @freshmen.update_attributes(freshmen_params)
+
+    if @freshmen.save
+      redirect_to freshmen_path(@freshmen)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @freshmen = Freshmen.find(params[:id])
+    @pictures = Picture.where(freshmen_id: @freshmen.id)
+    @pictures.each do |picture|
+      picture.destroy
+    end
+    @freshmen.destroy
+    redirect_to home_path
+  end
+
+  private def freshmen_params
+    params.require(:freshmen).permit(:category_id, :name, :description)
+  end
 end
