2ca00cc816541f7707e7330262f49296980a49b4
Ryvan Firdy <ryvanfirdy@gmail.com>
2018-08-18 12:34:03 +0800

    add ckeditor to event

--- app/models/ckeditor/attachment_file.rb
+++ app/models/ckeditor/attachment_file.rb
@@ -28,7 +28,7 @@ module Ckeditor
                       /:filename'
 
     validates_attachment_presence :data
-    validates_attachment_size :data, less_than: 100.megabytes
+    validates_attachment_size :data, less_than: 10.megabytes
     do_not_validate_attachment_file_type :data
 
     def url_thumb
